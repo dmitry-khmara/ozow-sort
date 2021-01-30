471e62565aa21f1d7bb9a5f0f50bc7a744f9e1d4
Dmitry Khmara <dmitry.khmara@gmail.com>
2021-01-30 18:13:50 +0200

    Added a more time-efficient way to sort English strings.

--- src/Ozow.Sort/Sorter.cs
+++ src/Ozow.Sort/Sorter.cs
@@ -1,20 +1,57 @@
-﻿using System;
-using System.Linq;
+﻿using System.Collections.Generic;
+using System.Text;
 
 namespace Ozow.Sort
 {
     public class Sorter
     {
+        private static readonly char[] EnglishAlphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+
         public string Sort(string stringToSort)
         {
             if (stringToSort == null)
                 return null;
-            
-            var characters = stringToSort.ToCharArray();
 
-            var sortedCharacters = characters.Where(IsEnglishCharacter).Select(Char.ToLowerInvariant).OrderBy(o => o);
+            var charCounter = new Dictionary<char, int>();
+            var totalEnglishCharacters = 0;
 
-            return String.Join("", sortedCharacters);
+            foreach (var character in stringToSort)
+            {
+                if (!IsEnglishCharacter(character))
+                    continue;
+
+                var lowercaseCharacter = char.ToLowerInvariant(character);
+
+                IncreaseCounterForCharacter(charCounter, lowercaseCharacter);
+
+                totalEnglishCharacters++;
+            }
+
+            var sortedString = AssembleCounterToString(charCounter, totalEnglishCharacters);
+
+            return sortedString;
+        }
+
+        private static void IncreaseCounterForCharacter(Dictionary<char, int> charCounter, char lowercaseCharacter)
+        {
+            charCounter.TryGetValue(lowercaseCharacter, out int currentCharCounter);
+
+            charCounter[lowercaseCharacter] = currentCharCounter + 1;
+        }
+
+        private string AssembleCounterToString(Dictionary<char, int> charCounter, int length)
+        {
+            var stringBuilder = new StringBuilder(length);
+
+            foreach (var letter in EnglishAlphabet)
+            {
+                if (!charCounter.TryGetValue(letter, out int counterForThisLetter))
+                    continue;
+
+                stringBuilder.Append(letter, counterForThisLetter);
+            }
+
+            return stringBuilder.ToString();
         }
 
         private static bool IsEnglishCharacter(char character)
